b35a072fe16e869d581a12a3c5c75b565dae5ab4
ToGo347 <togo347@gmail.com>
2019-01-18 00:12:10 +0100

    minimize

--- proyecto final/red.py
+++ proyecto final/red.py
@@ -4,6 +4,7 @@ from pandas.io.parsers import read_csv
 import scipy.optimize as opt
 #from sklearn.preprocessing import PolynomialFeatures as pf
 from scipy.io import loadmat
+#import pdb; pdb.set_trace()
 
 def loadMatrix(file):
 	return np.loadtxt(file)
@@ -15,17 +16,16 @@ def derivadaSigmoide(z):
 	return sigmoide(z)*(1-sigmoide(z))
 
 def pesosAleatorios(L_in, L_out):
-	return np.random.rand(L_out, 1 + L_in) * 0.24 - 0.12
+	epsilon = 6**(0.5) / (L_in + L_out)
+	return np.random.rand(L_in, L_out + 1) * 2 * epsilon - epsilon
 
 def backprop(params_rn, num_entradas, num_ocultas , num_etiquetas , X, y, reg):
-
 	theta1 = np.reshape(params_rn[:num_ocultas*(num_entradas + 1)], (num_ocultas, (num_entradas + 1)))
 	theta2 = np.reshape(params_rn[num_ocultas*(num_entradas + 1):], (num_etiquetas, (num_ocultas + 1)))
 	m = len(X)
 	#Input
 	ones_columns_input = np.array(np.ones(m))
 	a1 = np.insert(X, 0,ones_columns_input, axis = 1)
-
 	#hidden_layer
 	z2 = np.dot(theta1, a1.transpose())
 	a2 = sigmoide(z2)
@@ -36,8 +36,8 @@ def backprop(params_rn, num_entradas, num_ocultas , num_etiquetas , X, y, reg):
 	z3 = np.dot(theta2, a2)
 	h = sigmoide(z3)
 	y_converted = np.zeros(h.shape)
-	for i in range (0,len(X)): 
-	    y_converted[np.ravel(y)[i]-1][i] = 1
+	for i in range(0,len(X)):
+		y_converted[np.ravel(y)[i]-1][i] = 1
 
 	 #Cost
 	regulation = (reg/(2*m)) * (np.sum(theta1**2) + np.sum(theta2**2))
@@ -55,25 +55,34 @@ def backprop(params_rn, num_entradas, num_ocultas , num_etiquetas , X, y, reg):
 	delta1 = np.dot(d2, a1)
 	                              
 	#Regularization
-
 	D1 = (delta1[1:,:]/m + theta1*reg/m).ravel()
 	D2 = (delta2/m + theta2*reg/m).ravel()
 	gradient = np.r_[D1, D2]
-
+	print J_regulated
 	return J_regulated, gradient
+
+def parametros(params, input_size, hidden_size, num_labels, X, y, reg):
+	result = opt.minimize(fun=backprop, x0=params,
+						 args=(input_size, hidden_size,
+						 num_labels, X, y, reg),
+						 method='TNC', jac=False,
+						 options={'maxiter': 70})
+	return result.x
 
 def test():
 	reg = 1
 	X = loadMatrix('arcene_train.data')
 	y = loadMatrix('arcene_train.labels')
-	print y.shape
+	y = y.astype(int)
 	num_entradas = X.shape[1]
 	num_ocultas = X.shape[1]/2
 	num_etiquetas = 2
-	theta1 = pesosAleatorios(X.shape[0], num_entradas)
-	theta2 = pesosAleatorios(X.shape[0], num_ocultas)
+	theta1 = pesosAleatorios(num_ocultas, X.shape[1])
+	theta2 = pesosAleatorios(num_etiquetas, num_ocultas)
 
 	#theta1, theta2 = weights[ 'Theta1' ], weights[ 'Theta2' ]
-	coste, gradiente = backprop(np.concatenate((np.ravel(theta1), np.ravel(theta2))), num_entradas, num_ocultas, num_etiquetas, X, y, reg)
+	#coste, gradiente = backprop(np.concatenate((np.ravel(theta1), np.ravel(theta2))), num_entradas, num_ocultas, num_etiquetas, X, y, reg)
+	gradiente = parametros(np.concatenate((np.ravel(theta1), np.ravel(theta2))), num_entradas, num_ocultas, num_etiquetas, X, y, reg)
+	print gradiente
 
 test()
